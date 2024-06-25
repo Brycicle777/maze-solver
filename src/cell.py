f9256249e4cc093457742e76380bdee22b7b81dc
Brycicle777 <BryceHargis@outlook.com>
2024-06-24 21:04:25 -0700

    Add cell class

---
+++ src/cell.py
@@ -0,0 +1,34 @@
+from tkinter import Tk, BOTH, Canvas
+from point import Point
+from line import Line
+
+class Cell:
+    def __init__(self, x1, x2, y1, y2, win, has_left_wall=True, has_right_wall=True, has_top_wall=True, has_bottom_wall=True):
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = win
+
+    def draw(self):
+        if self.has_left_wall:
+            Line(Point(self._x1, self._y1), Point(self._x1, self._y2)).draw(self._win.canvas, "black")
+        if self.has_right_wall:
+            Line(Point(self._x2, self._y1), Point(self._x2, self._y2)).draw(self._win.canvas, "black")
+        if self.has_top_wall:
+            Line(Point(self._x1, self._y1), Point(self._x2, self._y1)).draw(self._win.canvas, "black")
+        if self.has_bottom_wall:
+            Line(Point(self._x1, self._y2), Point(self._x2, self._y2)).draw(self._win.canvas, "black")
+
+
+
+
+
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 from window import Window
 from point import Point
 from line import Line
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    line_one = Line(Point(200, 50), Point(600, 50))
-    win.draw_line(line_one, "red")
-    line_two = Line(Point(400, 50), Point(400, 500))
-    win.draw_line(line_two, "red")
+#    line_one = Line(Point(200, 50), Point(600, 50))
+    #win.draw_line(line_one, "red")
+    #line_two = Line(Point(400, 50), Point(400, 500))
+    #win.draw_line(line_two, "red")
+    test_cell = Cell(50, 750, 50, 400, win)
+    test_cell.draw()
     win.wait_for_close()
 
 main()
