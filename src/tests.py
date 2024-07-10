23d859fb8d4e9247f00c241c0e3c3cc61db39bbb
Brycicle777 <BryceHargis@outlook.com>
2024-07-09 20:25:38 -0700

    Test entrance and exit removal

--- src/tests.py
+++ src/tests.py
@@ -17,6 +17,28 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
+    def test_entrance_and_exit_removal(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+
+        self.assertEqual(
+            m1._cells[0][0].has_top_wall,
+            False
+        )
+        self.assertEqual(
+            m1._cells[0][0].has_left_wall,
+            True
+        )
+        self.assertEqual(
+            m1._cells[-1][-1].has_bottom_wall,
+            False
+        )
+        self.assertEqual(
+            m1._cells[-1][-1].has_right_wall,
+            True
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
