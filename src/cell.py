2351081f75d4223d1eb2be8aac6a1f2a821baa38
Brycicle777 <BryceHargis@outlook.com>
2024-07-09 20:07:13 -0700

    Make win param optional

--- src/cell.py
+++ src/cell.py
@@ -4,7 +4,7 @@ from line import Line
 
 
 class Cell:
-    def __init__(self, x1, x2, y1, y2, win, has_left_wall=True, has_right_wall=True, has_top_wall=True, has_bottom_wall=True):
+    def __init__(self, x1, x2, y1, y2, win=None, has_left_wall=True, has_right_wall=True, has_top_wall=True, has_bottom_wall=True):
         self.has_left_wall = has_left_wall
         self.has_right_wall = has_right_wall
         self.has_top_wall = has_top_wall
@@ -16,24 +16,26 @@ class Cell:
         self._win = win
 
     def draw(self):
-        if self.has_left_wall:
-            Line(Point(self._x1, self._y1), Point(self._x1, self._y2)).draw(
-                self._win.canvas, "black")
-        if self.has_right_wall:
-            Line(Point(self._x2, self._y1), Point(self._x2, self._y2)).draw(
-                self._win.canvas, "black")
-        if self.has_top_wall:
-            Line(Point(self._x1, self._y1), Point(self._x2, self._y1)).draw(
-                self._win.canvas, "black")
-        if self.has_bottom_wall:
-            Line(Point(self._x1, self._y2), Point(self._x2, self._y2)).draw(
-                self._win.canvas, "black")
+        if self._win:
+            if self.has_left_wall:
+                Line(Point(self._x1, self._y1), Point(self._x1, self._y2)).draw(
+                    self._win.canvas, "black")
+            if self.has_right_wall:
+                Line(Point(self._x2, self._y1), Point(self._x2, self._y2)).draw(
+                    self._win.canvas, "black")
+            if self.has_top_wall:
+                Line(Point(self._x1, self._y1), Point(self._x2, self._y1)).draw(
+                    self._win.canvas, "black")
+            if self.has_bottom_wall:
+                Line(Point(self._x1, self._y2), Point(self._x2, self._y2)).draw(
+                    self._win.canvas, "black")
 
     def draw_move(self, to_cell, undo=False):
-        from_cell_center_point = Point(
-            (self._x2 - self._x1) / 2 + self._x1, (self._y2 - self._y1) / 2 + self._y1)
-        to_cell_center_point = Point(
-            (to_cell._x2 - to_cell._x1) / 2 + to_cell._x1, (to_cell._y2 - to_cell._y1) / 2 + to_cell._y1)
-        fill_color = "red" if undo is False else "gray"
-        Line(from_cell_center_point, to_cell_center_point).draw(
-            self._win.canvas, fill_color)
+        if self._win:
+            from_cell_center_point = Point(
+                (self._x2 - self._x1) / 2 + self._x1, (self._y2 - self._y1) / 2 + self._y1)
+            to_cell_center_point = Point(
+                (to_cell._x2 - to_cell._x1) / 2 + to_cell._x1, (to_cell._y2 - to_cell._y1) / 2 + to_cell._y1)
+            fill_color = "red" if undo is False else "gray"
+            Line(from_cell_center_point, to_cell_center_point).draw(
+                self._win.canvas, fill_color)
